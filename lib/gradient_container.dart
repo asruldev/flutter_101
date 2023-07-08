7f6beea17ad6a085ea038c0f95486de611ad9683
Asrul Harahap <admin@asrul.dev>
2023-07-08 15:54:27 +0700

    constructor variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 
+import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
@@ -9,18 +12,12 @@ class GradientContainer extends StatelessWidget {
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.purple, Colors.pink],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello App',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
+        child: StyledText("Asrul H"),
       ),
     );
   }
