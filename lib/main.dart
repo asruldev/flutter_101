8f034507294b1006587471cdda070a0754a5a10f
Asrul Harahap <admin@asrul.dev>
2023-07-08 13:19:07 +0700

    :fire: first app with flutter refresh learning

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple, Colors.pink],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Text('Hello Ap'),
+        ),
+      ),
+    ),
+  ));
+}
